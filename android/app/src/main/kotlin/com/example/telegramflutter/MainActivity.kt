a3b5c2001d6d0e90698bca50de1baa931637df02
parham <135715118+iamparhamohhh@users.noreply.github.com>
2026-02-03 22:19:03 +0330

    first commit by me

---
+++ android/app/src/main/kotlin/com/example/telegramflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.telegramflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:telegramflutter/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: RootApp()));
+}
